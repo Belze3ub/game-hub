1050ff2e5d7a00f172db2da8ec3eb20d43c5a3c7
Mikolaj Gawlik <mikolaj3313@gmail.com>
2023-07-19 19:22:58 +0200

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,8 @@
-import { Button, HStack, Image, List, ListItem } from '@chakra-ui/react';
+import { Button, HStack, Heading, Image, List, ListItem } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 import { Spinner } from '@chakra-ui/react';
+import GameHeading from './GameHeading';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
@@ -14,29 +15,36 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (error) return null;
 
   return (
-    <List>
-      {isLoading && <Spinner />}
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              boxSize="2rem"
-              objectFit="cover"
-              borderRadius="10px"
-            />
-            <Button
-              onClick={() => onSelectGenre(genre)}
-              variant="link"
-              fontWeight={genre.id === selectedGenre?.id ? '700' : '400'}
-              fontSize="lg"
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading as="h2" fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {isLoading && <Spinner />}
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="2rem"
+                objectFit="cover"
+                borderRadius="10px"
+              />
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                variant="link"
+                fontWeight={genre.id === selectedGenre?.id ? '700' : '400'}
+                fontSize="lg"
+                whiteSpace="normal"
+                textAlign="left"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
