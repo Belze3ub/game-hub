cf85d9aadfb2893ffdca8e7defe6b9abeaf80670
Mikolaj Gawlik <mikolaj3313@gmail.com>
2024-05-08 00:48:41 +0200

    Build game trailer

--- src/components/GameDetailsPage.tsx
+++ src/components/GameDetailsPage.tsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 import useGame from '../hooks/useGame';
 import ExpandableText from './ExpandableText';
 import GameAttributes from './GameAttributes';
+import GameTrailer from './GameTrailer';
 
 const GameDetailsPage = () => {
   const { slug } = useParams();
@@ -19,6 +20,7 @@ const GameDetailsPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
